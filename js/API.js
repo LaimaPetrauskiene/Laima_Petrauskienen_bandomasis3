985d62394b78e14b831a0be7cdec91f6c206ef8d
Laima Petrauskiene <laima@tinklai.net>
2021-11-15 12:02:36 +0200

    5: API failas

---
+++ js/API.js
@@ -0,0 +1,34 @@
+const baseURL = "http://localhost:3000";
+
+
+class API {
+
+
+    static fetchFurniture = (success, failure) => {
+        fetch(`${baseURL}/furniture`)
+            .then(res => res.json())
+            .then(success)
+            .catch(failure)
+    }
+
+    static deleteFurniture = (id, success, failure) => {
+        fetch(`${baseURL}/furniture/${id}`, {method: 'DELETE'})
+        .then(res => res.status === 200 ? success() : failure(res.statusText))
+        .catch(failure)
+    }
+
+
+}
+
+
+// API.fetchFurniture(
+ //   console.log,
+ //   console.error
+// )
+
+
+// API.deleteFurniture(
+//     "2",
+//     () => console.log('Ištrinta sėkmingai'),
+//     console.error
+// )
