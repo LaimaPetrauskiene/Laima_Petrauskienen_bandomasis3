4469d547cb8ef51654bb96fc95ef97249f406c2a
Laima Petrauskiene <laima@tinklai.net>
2021-11-15 12:09:29 +0200

    6: gaunami duomenys

--- js/components/FurnitureGridComponent.js
+++ js/components/FurnitureGridComponent.js
@@ -1,10 +1,45 @@
-class FurnitureGridComponent{
+class FurnitureGridComponent {
     constructor() {
+        this.state = {
+            loading: false,
+            furniture: []
+        }
         this.init();
     }
 
     init = () => {
+        this.state.loading = true;
+        this.initFetch();
         this.htmlElement = document.createElement('div');
-        this.htmlElement.innerHTML = 'Aš esu FurnitureGrid';
+        this.render();
+    }
+
+    initFetch = () => {
+        API.fetchFurniture(
+            (furniture) => {
+                this.state.loading = false;
+                this.saveFurniture(furniture);
+            },
+            (err) => {
+                alert(err);
+                this.state.loading = false;
+                this.render();
+            }
+        );
+    }
+
+    saveFurniture = (furniture) => {
+        this.state.furniture = furniture;
+        this.state.loading = false;
+        this.render();
+    }
+
+    render = () => {
+        const { loading, furniture } = this.state;
+        if (loading) {
+            this.htmlElement.innerHTML = 'Siunčiama...';
+        } else {
+            this.htmlElement.innerHTML = 'parsiųsta!';
+        }
     }
 }
